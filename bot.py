0340fe1b04308d06fc49e5cb2dd1a15e5f3b265f
SkamchikWqqq <paranoiaattack1@gmail.com>
2026-03-27 03:06:03 +0200

    Remove unused MemoryStorage import from bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import string
 import sqlite3
 from datetime import datetime
 from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram import Bot, Dispatcher, types
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import (
