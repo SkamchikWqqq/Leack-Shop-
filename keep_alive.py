5a0efb3012bf25d896dddaf571de52949fe792af
SkamchikWqqq <paranoiaattack1@gmail.com>
2026-03-27 02:25:15 +0200

    Update Flask app to run in threaded mode
    Enable threaded mode for Flask app and set thread as daemon.

--- keep_alive.py
+++ keep_alive.py
@@ -1,14 +1,15 @@
 from flask import Flask
 import threading
 
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'Bot is running!'
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, threaded=True)
 
 t = threading.Thread(target=run)
+t.daemon = True  # Это сделает поток демоном, чтобы он завершился с программой
 t.start()
