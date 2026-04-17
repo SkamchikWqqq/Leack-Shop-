8da4d16e9a4502aee251dc6fac527442b6b55087
SkamchikWqqq <paranoiaattack1@gmail.com>
2026-04-18 01:18:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -79,8 +79,7 @@ def start_bot_in_thread():
 # Запуск Flask и бота в отдельных потоках
 if __name__ == "__main__":
     Thread(target=run_flask, daemon=True).start()
-    Thread(target=start_bot_in_thread).start()   # Запускаем Telegram-бота
-    
+
 # ============================================================
 # БД
 # ============================================================
