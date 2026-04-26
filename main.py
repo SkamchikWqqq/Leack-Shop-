91cd94b7e07f5d6e6668358c7d255e3d8d7b61b5
SkamchikWqqq <paranoiaattack1@gmail.com>
2026-04-26 22:17:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -273,6 +273,9 @@ def main_menu_kb(user_id=None):
         builder.row(InlineKeyboardButton(text="📣 Рассылка", callback_data="broadcast"))
         builder.row(InlineKeyboardButton(text="🎟 Генерация промо", callback_data="gen_promo"))
     
+builder.row(InlineKeyboardButton(text="➕ Добавить ОП", callback_data="admin_add_channel"))
+        builder.row(InlineKeyboardButton(text="❌ Удалить ОП", callback_data="admin_list_channels"))
+
     return builder.as_markup()
 
 def catalog_kb():
