7763d3250c276f2b5cf742a52fb605926bba049f
SkamchikWqqq <paranoiaattack1@gmail.com>
2026-05-13 16:11:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def run_flask():
     app.run(host='0.0.0.0', port=port)
 
 # 3. ТВОИ НАСТРОЙКИ (КОНСТАНТЫ)
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8798655968:AAFS1WJYtTzWwg8cDB1kwdTOLTsneZVNmM0")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8798655968:AAEBK9gzVpUgQaW09vWKPgeEb7CIuteSPO8")
 CRYPTOBOT_API_TOKEN = os.getenv("CRYPTOBOT_API_TOKEN", "553441:AAd905Dra8Qp1GdSHuBbnWJNj8DfZYIXljf")
 
 ADMIN_IDS = []
